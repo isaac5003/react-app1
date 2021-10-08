ed7aaf53f1a0c3459aee1a86dba6a856b1a11dea
isaac <isaac.cortez1294@hotmail.com>
2021-10-07 18:25:24 -0600

    Peticiones http cambiadas a https

--- src/helpers/getGift.js
+++ src/helpers/getGift.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=dYWlKy6gYdZIcEad73KDUC6wGEj2ATrc&q=${encodeURI(category)}&limit=5`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=dYWlKy6gYdZIcEad73KDUC6wGEj2ATrc&q=${encodeURI(category)}&limit=5`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifts = data.map(img => {
